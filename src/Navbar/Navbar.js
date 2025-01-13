9654e0f1f67dd66ff417f8d5c012546f08822d8e
ZuiHydraClique <minetronde@gmail.com>
2025-01-13 19:03:59 +0100

    Hamburgermenü hinzugefügt

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import './Navbar.css';
-import '../styles/flex.css';
-import Icon from '../pictures/logo512.png';
+import React, { useState } from "react";
+import "./Navbar.css";
+import "../styles/flex.css";
+import Icon from "../pictures/logo512.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const scrollToSection = (id) => {
-        const element = document.getElementById(id);
-        if (element) {
-            const headerOffset = window.innerWidth < 1350 ? 0 : 70; // Höhe der Navbar oder anderen Elementen
-            const elementPosition = element.getBoundingClientRect().top;
-            const offsetPosition = elementPosition + window.scrollY - headerOffset;
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: 'smooth'
-            });
-        }
-    };
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const headerOffset = window.innerWidth < 1350 ? 0 : 70; // Höhe der Navbar
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.scrollY - headerOffset;
 
-    return (
-        <div
-            id="navbar-container"
-        >
-            <div
-                id="navbar-content"
-            >
-                <img src={Icon} id="navbar-logo"  onClick={() => scrollToSection('logo-container')} alt="ICON"/>
-                <div className="navbar-link" onClick={() => scrollToSection('bandmitglieder')}>
-                    Bandmitglieder
-                </div>
-                <div className="navbar-link" onClick={() => scrollToSection('seventees')}>
-                    70's
-                </div>
-                <div className="navbar-link" onClick={() => scrollToSection('jazzstandard')}>
-                    Jazz Standards
-                </div>
-                <div className="navbar-link" onClick={() => scrollToSection('latinjazz')}>
-                    Latin Jazz
-                </div>
-                <div className="navbar-link" onClick={() => scrollToSection('poprockclassic')}>
-                    Pop-Rock-Classic
-                </div>
-                <div className="navbar-link" onClick={() => scrollToSection('kontakt')}>
-                    Kontakt
-                </div>
-                <div className="navbar-link" onClick={() => scrollToSection('faqs')}>
-                    FAQs
-                </div>
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
 
-            </div>
+  return (
+    <div id="navbar-container">
+      
+      <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleNavbar}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`navbar-content menu ${isOpen ? "open" : ""}`}>
+        <img
+          src={Icon}
+          id="navbar-logo"
+          onClick={() => scrollToSection("logo-container")}
+          alt=""
+        />
+        <div className="navbar-link" onClick={() => scrollToSection("bandmitglieder")}>
+          Bandmitglieder
         </div>
-    );
+        <div className="navbar-link" onClick={() => scrollToSection("seventees")}>
+          70's
+        </div>
+        <div className="navbar-link" onClick={() => scrollToSection("jazzstandard")}>
+          Jazz Standards
+        </div>
+        <div className="navbar-link" onClick={() => scrollToSection("latinjazz")}>
+          Latin Jazz
+        </div>
+        <div className="navbar-link" onClick={() => scrollToSection("poprockclassic")}>
+          Pop-Rock-Classic
+        </div>
+        <div className="navbar-link" onClick={() => scrollToSection("kontakt")}>
+          Kontakt
+        </div>
+        <div className="navbar-link" onClick={() => scrollToSection("faqs")}>
+          FAQs
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
